b1196a1830342c9564d8778f151188c5a27401bf
igor <igor_miazek@protonmail.com>
2021-10-08 14:56:48 +0200

    init

---
+++ src/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+
+setuptools.setup(
+    name='rttmongo',
+    version='',
+    description='',
+    long_description='',
+    author='Igor Miazek',
+    url='',
+    download_url='https://github.com/TECHS-Technological-Solutions/rtt-mongo',
+    license='MIT',
+    namespace_packages=[],
+    package_dir={'': '.'},
+    packages=[
+        'rttmongo'
+    ],
+    include_package_data=True,
+    install_requires=[
+
+    ],
+    entry_points={
+        "console_scripts": [""]
+    }
+)
